4c8e99b8e5982f08bf40c9b9df4adf00b1c743e9
Roman Serravalle <romanserravalle@gmail.com>
2023-09-20 03:36:11 -0300

    Agregados comentarios al archivo .js

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,11 @@
+//Llamados HTML
+
 const elCartera = document.querySelector("#cartera span");
 const elCarrito = document.querySelector("#carrito");
 const elTotal = document.querySelector("#total span");
 const elBienvenida = document.querySelector("h1");
 
+//Objetos productos
 class Producto {
   constructor(nombre, precio) {
     this.nombre = nombre;
@@ -17,8 +20,8 @@ const cpuAmd = new Producto("AMD Ryzen 5 7600", 150000);
 const ramCorsair = new Producto("Memoria RAM Corsair DDR4 8GB", 25000);
 const ramViper = new Producto("Memoria RAM Patriot Viper DDR4 8GB", 30000);
 
-let nombreUsuario = prompt("Nombre de usuario: ");
-
+//Variables de usuario y dinero en cartera
+const nombreUsuario = prompt("Nombre de usuario: ");
 let cartera = 800000;
 
 elCartera.innerText = "$" + cartera;
@@ -27,15 +30,14 @@ elTotal.innerText = "$" + 0;
 elBienvenida.innerText =
   "Bienvenido a Velen Computación, " + nombreUsuario + "!";
 
+//Array del carrito
 const carrito = [];
 
+//Función de compra
 function comprar(producto) {
   if (cartera - producto.precio >= 0) {
     carrito.push(producto);
     cartera -= producto.precio;
-    console.clear();
-    console.log(carrito);
-    console.log("Dinero en cartera: ", cartera);
     actualizar();
   } else {
     alert(
@@ -44,6 +46,7 @@ function comprar(producto) {
   }
 }
 
+//Función de venta
 function vender(nombreProducto) {
   const productoCarrito = carrito.find(
     (producto) => producto.nombre == nombreProducto
@@ -52,14 +55,17 @@ function vender(nombreProducto) {
   if (productoCarrito) {
     cartera += productoCarrito.precio;
     carrito.splice(carrito.indexOf(productoCarrito), 1);
-    console.clear();
-    console.log(carrito);
-    console.log("Dinero en cartera: ", cartera);
     actualizar();
   }
 }
 
+//Función para actualizar el HTML
 function actualizar() {
+  console.clear();
+  console.log("Productos en carrito: ", carrito);
+  console.log("Dinero en la cartera: ", cartera);
+
+  //Función para calcular el valor total del carrito
   const totalCarrito = carrito.reduce((acc, el) => acc + el.precio, 0);
   console.log("Valor total del carrito: $" + totalCarrito);
   elCarrito.innerHTML = "";
